639904e46f31ad3ca6358b5a5b1064c8419c0444
Berat Çimen <beratcmn@gmail.com>
2021-11-14 22:17:30 +0300

    Added new functions

--- src/mithen/mithen.py
+++ src/mithen/mithen.py
@@ -1,3 +1,22 @@
-class All:
-    def karekok(x):
-        return x**2
+def kare(x):
+    return x**2
+
+
+def karekok(x):
+    return x**(1/2)
+
+
+def kup(x):
+    return x**3
+
+
+def kupkok(x):
+    return x**(1/3)
+
+
+def mutlak(x):
+    return abs(x)
+
+
+def ussu(x, y):
+    return x**y
